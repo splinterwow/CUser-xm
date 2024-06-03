62145bd437981f435a36a9f641080c2f75207d85
splinter wow <splinterwow01@gmail.com>
2024-06-03 14:40:43 +0500

    vazifa bajarildi

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>Copyright © 2024 - All right reserved by ACME Industries Ltd. <a href="#">Sherzodbek Adkhamjonov</a></p>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
